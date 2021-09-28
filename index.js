db275d7a10c5b6fa9fa1a1d76e736ab383c9168f
druparelqa <druparel@miraclesoft.com>
2021-09-28 18:24:18 -0400

    formatted

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-//document.getElementById("count-el").innerText = 5
-
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
 let count = 0;
 
 function increment() {
     count += 1;
-    countEl.innerText = count;
+    countEl.innerText = count
 }
 
 function save() {
