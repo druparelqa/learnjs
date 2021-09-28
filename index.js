ee11e2353282fc128e1ec0fa7b55ef6df034c1c1
druparelqa <druparel@miraclesoft.com>
2021-09-28 18:14:11 -0400

    complete js app

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 //document.getElementById("count-el").innerText = 5
 
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 let count = 0;
 
 function increment() {
-    count = count + 1;
+    count += 1;
     countEl.innerText = count;
 }
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    count = 0
+    countEl.innerText = count
+}
+
+
