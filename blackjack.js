fe046332515023bcb1b9933df1967654e37d92fa
druparelqa <druparel@miraclesoft.com>
2021-09-30 16:37:00 -0400

    added new files for blackjack

--- blackjack.js
+++ blackjack.js
@@ -1,20 +1,34 @@
-let firstCard = 11
-let secondCard = 11
+let firstCard = 10
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+//let sumEl = document.getElementById("sum-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 
-if (sum <= 20) {
-    console.log("Do you want to draw a new card?")
-} else if (sum === 21){
-    console.log("Wohoo! you got Blackjack!")
-    hasBlackJack = true
-} else {
-    console.log("You are out of the game")
-    isAlive = false
+function startGame() {
+    cardsEl.textContent = "Cards:" + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21){
+        message ="You got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message ="You are out of the game"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+
 }
-message = "we are logging out"
-console.log(hasBlackJack)
-console.log(isAlive)
-console.log(message)
+
+function newCard() {
+    let newCard = 7
+    sum += newCard
+    startGame()
+}
